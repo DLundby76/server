a7f1544c6e26eba1716e425a9e8fc44062ff8f93
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-07-31 19:21:17 -0600

    i didn't need this repo

--- twoserver.js
+++ twoserver.js
@@ -1,1 +1,15 @@
 var http = require('http');
+
+var port = 7000;
+var port = 7500;
+
+
+function handleRequestOne(request, response)
+{
+  response.end("you're awesome-sauce");
+  }
+
+  function handleRequestTwo(request, response)
+  {
+    response.end("you suck monkey balls");
+    }
