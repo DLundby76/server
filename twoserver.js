bdb36f4998f63404b7758df19cb1e57fc2721ec0
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-07-31 19:28:07 -0600

    boo janet

--- twoserver.js
+++ twoserver.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-var port = 7000;
-var port = 7500;
+var PORTONE = 7000;
+var PORTTWO = 7500;
 
 
 function handleRequestOne(request, response)
@@ -13,3 +13,6 @@ function handleRequestOne(request, response)
   {
     response.end("you suck monkey balls");
     }
+
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
